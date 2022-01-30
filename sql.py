e6e59762bac2a5fef592bba7849d8449074d9787
Rawan Almobarak <rawan.almobarak@gmail.com>
2022-01-30 12:43:42 +0300

    Add files via upload

--- sql.py
+++ sql.py
@@ -13,92 +13,100 @@ mycursor = mydb.cursor()
 
 
 
-#--------------------------------------- database -------------------------------------
+# --------------------------------------- database -------------------------------------
 
-#mycursor.execute("CREATE DATABASE mydatabase")
-#mycursor.execute("SHOW DATABASES")
+mycursor.execute("CREATE DATABASE mydatabase")
+mycursor.execute("SHOW DATABASES")
 
-#for x in mycursor:
-#  print(x)
-
-
-#--------------------------------------- TABLE --------------------------------------
-
-# mycursor.execute("CREATE TABLE customers (name VARCHAR(255), address VARCHAR(255))")
+for x in mycursor:
+ print(x)
 
 
-#mycursor.execute("DROP TABLE IF EXISTS customers")
+# --------------------------------------- creating TABLE --------------------------------------
 
-# mycursor.execute("CREATE TABLE customers (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), address VARCHAR(255))")
-
-
-# mycursor.execute("SHOW TABLES")
+mycursor.execute("CREATE TABLE customers (name VARCHAR(255), address VARCHAR(255))")
 
 
-# for x in mycursor:
-#  print(x)
+mycursor.execute("DROP TABLE IF EXISTS customers")
+
+mycursor.execute("CREATE TABLE customers (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), address VARCHAR(255))")
+
+
+mycursor.execute("SHOW TABLES")
+
+
+for x in mycursor:
+ print(x)
+
+
+#--------------------------------------- Inserting one value to custmors table --------------------------------------
+
+
+sql = "INSERT INTO customers (name, address) VALUES (%s, %s)"
+val = ("Michelle", "Blue Village")
+mycursor.execute(sql, val)
+
+mydb.commit()
+
+print("1 record inserted, ID:", mycursor.lastrowid)
+
+
+mycursor.execute("SELECT * FROM customers")
+
+myresult = mycursor.fetchall()
+
+for x in myresult:
+  print(x)
+
+
+#--------------------------------------- Inserting to table --------------------------------------
+
+
+sql = "INSERT INTO customers (name, address) VALUES (%s, %s)"
+val = ("John", "Highway 21")
+mycursor.execute(sql, val)
+
+print(mycursor.rowcount, "record inserted.")
+
+val = [
+ ('Peter', 'Lowstreet 4'),
+ ('Amy', 'Apple st 652'),
+ ('Hannah', 'Mountain 21'),
+ ('Michael', 'Valley 345'),
+ ('Sandy', 'Ocean blvd 2'),
+ ('Betty', 'Green Grass 1'),
+ ('Richard', 'Sky st 331'),
+ ('Susan', 'One way 98'),
+ ('Vicky', 'Yellow Garden 2'),
+ ('Ben', 'Park Lane 38'),
+ ('William', 'Central st 954'),
+ ('Chuck', 'Main Road 989'),
+ ('Viola', 'Sideway 1633')
+]
+
+mycursor.executemany(sql, val)
+
+mydb.commit()
+
+print(mycursor.rowcount, "record inserted.")
 
 
 
 
 
-# sql = "INSERT INTO customers (name, address) VALUES (%s, %s)"
-# val = ("John", "Highway 21")
-# mycursor.execute(sql, val)
 
-# print(mycursor.rowcount, "record inserted.")
 
-# val = [
-#  ('Peter', 'Lowstreet 4'),
-#  ('Amy', 'Apple st 652'),
-#  ('Hannah', 'Mountain 21'),
-#  ('Michael', 'Valley 345'),
-#  ('Sandy', 'Ocean blvd 2'),
-#  ('Betty', 'Green Grass 1'),
-#  ('Richard', 'Sky st 331'),
-#  ('Susan', 'One way 98'),
-#  ('Vicky', 'Yellow Garden 2'),
-#  ('Ben', 'Park Lane 38'),
-#  ('William', 'Central st 954'),
-#  ('Chuck', 'Main Road 989'),
-#  ('Viola', 'Sideway 1633')
-# ]
+#--------------The fetchone() -------------- return the first row of the result
 
-# mycursor.executemany(sql, val)
+mycursor.execute("SELECT * FROM customers")
 
-# mydb.commit()
+myresult = mycursor.fetchone()
 
-# print(mycursor.rowcount, "record inserted.")
+print(myresult)
 
 
 
-#sql = "INSERT INTO customers (name, address) VALUES (%s, %s)"
-#val = ("Michelle", "Blue Village")
-#mycursor.execute(sql, val)
-
-#mydb.commit()
-
-#print("1 record inserted, ID:", mycursor.lastrowid)
-
-
-# mycursor.execute("SELECT * FROM customers")
-
-# myresult = mycursor.fetchall()
-
-# for x in myresult:
-#   print(x)
-
-
-
-
-#--------------The fetchone() -------- return the first row of the result
-
-# mycursor.execute("SELECT * FROM customers")
-
-# myresult = mycursor.fetchone()
-
-# print(myresult)
-
+#--------------------------------------- SELECT from Customers table --------------------------------------
 
 
 
@@ -113,7 +121,7 @@ mycursor = mydb.cursor()
 
 
 
-
+#--------------------------------------- another SELECT ends with way from Customers table --------------------------------------
 
 # sql = "SELECT * FROM customers WHERE address LIKE '%way%'"
 
@@ -140,7 +148,7 @@ mycursor = mydb.cursor()
 
 
 
-
+#---------------------------------------  SELECT and ORDER from Customers table --------------------------------------
 # sql = "SELECT * FROM customers ORDER BY name"
 
 # mycursor.execute(sql)
@@ -151,6 +159,7 @@ mycursor = mydb.cursor()
 #   print(x)
 
 
+#---------------------------------------  SELECT and DESC from Customers table --------------------------------------
 
 # sql = "SELECT * FROM customers ORDER BY name DESC"
 
@@ -162,6 +171,7 @@ mycursor = mydb.cursor()
 #   print(x)
 
 
+#---------------------------------------  DELETE from Customers table --------------------------------------
 
 # sql = "DELETE FROM customers WHERE address = 'Mountain 21'"
 
@@ -170,6 +180,7 @@ mycursor = mydb.cursor()
 # mydb.commit()
 
 # print(mycursor.rowcount, "record(s) deleted")
+
 
 
 
@@ -185,7 +196,7 @@ mycursor = mydb.cursor()
 
 
 
-
+#---------------------------------------  DROP  Customers table --------------------------------------
 
 # sql = "DROP TABLE customers"
 
@@ -202,6 +213,7 @@ mycursor = mydb.cursor()
 
 
 
+#---------------------------------------  UPDATE from Customers table --------------------------------------
 
 # sql = "UPDATE customers SET address = 'Canyon 123' WHERE address = 'Valley 345'"
 
@@ -226,6 +238,7 @@ mycursor = mydb.cursor()
 
 
 
+#---------------------------------------  SHOWING 5 records --------------------------------------
 
 # mycursor.execute("SELECT * FROM customers LIMIT 5")
 
@@ -238,6 +251,8 @@ mycursor = mydb.cursor()
 
 
 
+#---------------------------------------  SHOWING 5 records from row 2 --------------------------------------
+
 # mycursor.execute("SELECT * FROM customers LIMIT 5 OFFSET 2")
 
 # myresult = mycursor.fetchall()
@@ -249,6 +264,9 @@ mycursor = mydb.cursor()
 
 
 
+
+
+#---------------------------------------  creating % inserting users Table --------------------------------------
 
 
 # mycursor.execute("CREATE TABLE users (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), fav INT)")
@@ -276,29 +294,31 @@ mycursor = mydb.cursor()
 # mycursor.execute("CREATE TABLE products (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255))")
 
 
+#---------------------------------------  creating & inserting products Table --------------------------------------
+
+
+
+# sql = "INSERT INTO products (id, name) VALUES (%s, %s)"
+
+# val = [
+#  (154, 'Chocolate Heaven'),
+#  (155, 'Tasty Lemons'),
+#  (156,'Vanilla Dreams')
+# ]
 
 
 
 
-sql = "INSERT INTO products (id, name) VALUES (%s, %s)"
 
-val = [
- (154, 'Chocolate Heaven'),
- (155, 'Tasty Lemons'),
- (156,'Vanilla Dreams')
-]
+# mycursor.executemany(sql, val)
 
+# mydb.commit()
+
+# print(mycursor.rowcount, "record inserted.")
 
 
 
-
-mycursor.executemany(sql, val)
-
-mydb.commit()
-
-print(mycursor.rowcount, "record inserted.")
-
-
+#---------------------------------------  Join examples --------------------------------------
 
 # sql = "SELECT \
 #   users.name AS user, \
@@ -314,8 +334,8 @@ print(mycursor.rowcount, "record inserted.")
 #   print(x)
 
 
-sql = "SELECT \
-  users.name AS user, \
-  products.name AS favorite \
-  FROM users \
-  LEFT JOIN products ON users.fav = products.id"
+# sql = "SELECT \
+#   users.name AS user, \
+#   products.name AS favorite \
+#   FROM users \
+#   LEFT JOIN products ON users.fav = products.id"
